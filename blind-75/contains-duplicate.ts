7876b7e23f4c8a761f78789052763f3106a271fd
Marcio Alves <marcio.welben@gmail.com>
2026-02-11 14:05:15 -0300

    Valid anagram (easy)

--- blind-75/contains-duplicate.ts
+++ blind-75/contains-duplicate.ts
@@ -1,15 +1,15 @@
 /**
-*    Contains Duplicate (easy)
+* Contains Duplicate (easy)
 *
-*    Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+* Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
 *
-*    Example 1:
-*        Input: nums = [1, 2, 3, 3]
-*        Output: true
+* Example 1:
+*   Input: nums = [1, 2, 3, 3]
+*   Output: true
 *    
-*    Example 2:
-*        Input: nums = [1, 2, 3, 4]
-*        Output: false
+* Example 2:
+*   Input: nums = [1, 2, 3, 4]
+*   Output: false
 */
 
 // Time: O(n)
---
+++ blind-75/valid-anagram.ts
@@ -0,0 +1,37 @@
+/**
+ * Valid Anagram (easy)
+ * 
+ * Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+ * 
+ * An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+ * 
+ * Example 1:
+ *  Input: s = "racecar", t = "carrace"
+ *  Output: true
+ * 
+ * Example 2:
+ *  Input: s = "jar", t = "jam"
+ *  Output: false
+ * 
+ * Constraints:
+   's' and 't' consist of lowercase English letters.
+ */
+
+
+class Solution {
+
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) return false
+
+        const ocurr = new Array(26).fill(0)
+
+        for (let i = 0; i < s.length; i++) {
+            // Index by charCode - 97 ('a'); increment for s, decrement for t; anagram iff all stay 0
+            ocurr[s.charCodeAt(i) - 97] += 1
+            ocurr[t.charCodeAt(i) - 97] -= 1
+        }
+
+        return ocurr.every((c) => c === 0)
+    }
+
+}
